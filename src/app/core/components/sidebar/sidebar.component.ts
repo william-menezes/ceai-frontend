542b4aa851ad14dbd7476664e464359948ab06f4
William Damascena <williamenezes@outlook.com>
2024-04-08 12:24:28 -0300

    feat: add sidebar style

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,15 +1,42 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { ThemeService } from '../../../primeng/services/theme.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-  sidebarVisible1: boolean = false;
+export class SidebarComponent implements OnInit {
+  theme: boolean = true;
+  items: MenuItem[] | undefined;
   @Input() sidebarVisible: boolean = false;
-
   @Output() sidebarEmitter: EventEmitter<boolean> = new EventEmitter();
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Dashboard', icon: 'pi pi-home', routerLink: '' },
+      { label: 'Alunos', icon: 'pi pi-users', routerLink: '' },
+      { label: 'Funcionários', icon: 'pi pi-user', routerLink: '' },
+      { label: 'Horários', icon: 'pi pi-clock', routerLink: '' },
+      { label: 'Atividades', icon: 'pi pi-th-large', routerLink: '' },
+      { label: 'Turmas', icon: 'pi pi-calendar', routerLink: '' },
+      { label: 'Salas', icon: 'pi pi-building', routerLink: '' },
+      { label: 'Atestados', icon: 'pi pi-file', routerLink: '' },
+    ];
+  }
+
+  toggleTheme(theme: boolean) {
+    this.theme = !theme;
+
+    if (this.theme) {
+      this.themeService.switchTheme('lara-light-blue');
+    } else {
+      this.themeService.switchTheme('lara-dark-blue');
+    }
+  }
 
   onHide($event: any) {
     this.sidebarEmitter.emit(false);
