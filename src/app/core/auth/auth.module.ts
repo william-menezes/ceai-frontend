fb8be68d71dc32b790450075870bfa1f3d054312
William Menezes Damascena <williamenezes@outlook.com>
2024-04-12 00:48:58 -0300

    feat: login component style

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -1,21 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { PrimengModule } from '../../primeng/primeng.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent,
-    AuthComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginComponent, RegisterComponent, AuthComponent],
+  imports: [CommonModule, AuthRoutingModule, PrimengModule],
 })
-export class AuthModule { }
+export class AuthModule {}
