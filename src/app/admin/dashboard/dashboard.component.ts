730373be78f261c0945221275d9dc2b3d9cea947
William Damascena <williamenezes@outlook.com>
2024-04-30 12:26:35 -0300

    fix: improve dashboard component

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -7,15 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   userBySexData: any;
-  userBySexOptions: any;
-
   data: any;
-  options: any;
 
   ngOnInit(): void {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--text-color');
-    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const textColorSecondary = documentStyle.getPropertyValue(
+      '--text-color-secondary'
+    );
     const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
     this.userBySexData = {
@@ -23,81 +22,105 @@ export class DashboardComponent implements OnInit {
       datasets: [
         {
           data: [207, 804],
-          backgroundColor: ['rgba(59, 130, 246, 0.5)', 'rgba(236, 72, 153, 0.5)'],
+          backgroundColor: [
+            'rgba(59, 130, 246, 0.5)',
+            'rgba(236, 72, 153, 0.5)',
+          ],
           borderColor: ['rgba(59, 130, 246)', 'rgba(236, 72, 153)'],
-          hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--pink-400')],
+          hoverBackgroundColor: [
+            documentStyle.getPropertyValue('--blue-400'),
+            documentStyle.getPropertyValue('--pink-400'),
+          ],
           borderWidth: 1,
-          hoverBorderJoinStyle: 'miter' //'round'|'bevel'|'miter'
+          hoverBorderJoinStyle: 'miter', //'round'|'bevel'|'miter'
         },
       ],
-    };
-
-    this.userBySexOptions = {
-      plugins: {
-        legend: {
-          labels: {
-            usePointStyle: true,
+      options: {
+        plugins: {
+          legend: {
+            labels: {
+              usePointStyle: true,
+            },
+            align: 'center',
+            position: 'right',
           },
-          align: 'center',
-          position: 'right'
-        }
+        },
       }
     };
 
     this.data = {
-      labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+      labels: [
+        'Jan',
+        'Fev',
+        'Mar',
+        'Abr',
+        'Mai',
+        'Jun',
+        'Jul',
+        'Ago',
+        'Set',
+        'Out',
+        'Nov',
+        'Dez',
+      ],
       datasets: [
-          {
-              type: 'bar',
-              label: 'Masculino',
-              backgroundColor: documentStyle.getPropertyValue('--blue-500'),
-              data: [170, 15, 9, 13]
-          },
-          {
-              type: 'bar',
-              label: 'Feminino',
-              backgroundColor: documentStyle.getPropertyValue('--pink-500'),
-              data: [674, 58, 34, 38]
-          }
-      ]
-  };
-
-  this.options = {
-      maintainAspectRatio: false,
-      aspectRatio: 0.8,
-      plugins: {
+        {
+          type: 'bar',
+          label: 'Feminino',
+          backgroundColor: documentStyle.getPropertyValue('--blue-700'),
+          data: [, /* 674 */ 58, 34, 38],
+          borderRadius: 16,
+          barThickness: 12,
+        },
+        {
+          type: 'bar',
+          label: 'Masculino',
+          backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+          data: [, /* 170 */ 15, 9, 13],
+          borderRadius: 16,
+          barThickness: 12,
+        },
+      ],
+      options: {
+        maintainAspectRatio: false,
+        aspectRatio: 1.5,
+        plugins: {
           tooltip: {
-              mode: 'index',
-              intersect: false
+            mode: 'index',
+            intersect: true,
           },
           legend: {
-              labels: {
-                  color: textColor
-              }
-          }
-      },
-      scales: {
+            labels: {
+              color: textColor,
+              usePointStyle: true,
+            },
+            align: 'center',
+            position: 'bottom',
+          },
+        },
+        scales: {
           x: {
-              stacked: true,
-              ticks: {
-                  color: textColorSecondary
-              },
-              grid: {
-                  color: surfaceBorder,
-                  drawBorder: false
-              }
+            stacked: true,
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+              drawBorder: false,
+            },
           },
           y: {
-              stacked: true,
-              ticks: {
-                  color: textColorSecondary
-              },
-              grid: {
-                  color: surfaceBorder,
-                  drawBorder: false
-              }
-          }
-      }
-  };
+            stacked: true,
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+              drawBorder: false,
+            },
+          },
+        },
+      },
+    };
   }
 }
