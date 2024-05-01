1ab3c388f7f7278670b5302182ec3b13c36e2b59
William Menezes Damascena <williamenezes@outlook.com>
2024-05-01 18:33:59 -0300

    feat: add new chart in dashboard component

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   userBySexData: any;
+  studentsData: any;
   data: any;
+  months: string[] = [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez',
+  ];
 
   ngOnInit(): void {
     const documentStyle = getComputedStyle(document.documentElement);
@@ -23,16 +38,17 @@ export class DashboardComponent implements OnInit {
         {
           data: [207, 804],
           backgroundColor: [
-            'rgba(59, 130, 246, 0.5)',
-            'rgba(236, 72, 153, 0.5)',
+            /* 'rgba(59, 130, 246, 0.5)',
+            'rgba(236, 72, 153, 0.5)', */
+            documentStyle.getPropertyValue('--blue-500'),
+            documentStyle.getPropertyValue('--blue-700'),
           ],
-          borderColor: ['rgba(59, 130, 246)', 'rgba(236, 72, 153)'],
           hoverBackgroundColor: [
             documentStyle.getPropertyValue('--blue-400'),
-            documentStyle.getPropertyValue('--pink-400'),
+            documentStyle.getPropertyValue('--blue-600'),
           ],
-          borderWidth: 1,
-          hoverBorderJoinStyle: 'miter', //'round'|'bevel'|'miter'
+          borderWidth: 2,
+          hoverBorderJoinStyle: 'bevel', //'round'|'bevel'|'miter'
         },
       ],
       options: {
@@ -45,24 +61,11 @@ export class DashboardComponent implements OnInit {
             position: 'right',
           },
         },
-      }
+      },
     };
 
     this.data = {
-      labels: [
-        'Jan',
-        'Fev',
-        'Mar',
-        'Abr',
-        'Mai',
-        'Jun',
-        'Jul',
-        'Ago',
-        'Set',
-        'Out',
-        'Nov',
-        'Dez',
-      ],
+      labels: this.months,
       datasets: [
         {
           type: 'bar',
@@ -105,8 +108,81 @@ export class DashboardComponent implements OnInit {
               color: textColorSecondary,
             },
             grid: {
+              display: false,
+              //color: surfaceBorder,
+              //drawBorder: false,
+            },
+          },
+          y: {
+            stacked: true,
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
               color: surfaceBorder,
               drawBorder: false,
+            },
+          },
+        },
+      },
+    };
+
+    this.studentsData = {
+      labels: this.months,
+      datasets: [
+        {
+          label: 'Total de usuários masculinos',
+          data: [170, 185, 194, 207],
+          fill: false,
+          backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          tension: 0.5,
+        },
+        {
+          label: 'Total de usuários femininos',
+          data: [674, 732, 766, 804],
+          fill: false,
+          backgroundColor: documentStyle.getPropertyValue('--blue-700'),
+          borderColor: documentStyle.getPropertyValue('--blue-700'),
+          tension: 0.4,
+        },
+        {
+          label: 'Total de usuários',
+          data: [844, 917, 960, 1011],
+          fill: false,
+          //backgroundColor: 'rgba(7, 75, 185, 0.2)',
+          backgroundColor: documentStyle.getPropertyValue('--primary-900'),
+          borderColor: documentStyle.getPropertyValue('--primary-900'),
+          tension: 0.4,
+        },
+      ],
+      options: {
+        maintainAspectRatio: false,
+        aspectRatio: 1.5,
+        plugins: {
+          tooltip: {
+            mode: 'index',
+            intersect: true,
+          },
+          legend: {
+            labels: {
+              color: textColor,
+              usePointStyle: true,
+            },
+            align: 'center',
+            position: 'bottom',
+          },
+        },
+        scales: {
+          x: {
+            stacked: true,
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              display: false,
+              //color: surfaceBorder,
+              //drawBorder: false,
             },
           },
           y: {
