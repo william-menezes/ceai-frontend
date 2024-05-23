388a177b3c6f28b42199918151427be79820ba04
William Menezes Damascena <williamenezes@outlook.com>
2024-05-22 22:45:08 -0300

    feat: add phone and cpf pipe

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { PrimengModule } from '../primeng/primeng.module';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PhonePipe } from './pipes/phone.pipe';
+import { CpfPipe } from './pipes/cpf.pipe';
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent],
+  declarations: [HeaderComponent, SidebarComponent, PhonePipe, CpfPipe],
   imports: [CommonModule, PrimengModule],
-  exports: [HeaderComponent, SidebarComponent],
+  exports: [HeaderComponent, SidebarComponent, CpfPipe, PhonePipe],
 })
 export class CoreModule {}
